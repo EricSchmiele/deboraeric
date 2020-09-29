c63b1eb9932a53139d09ccbf6724826b0fbaa0d1
eric <eric@sonner.com.br>
2020-09-28 21:27:47 -0300

    Updates

---
+++ precache-manifest.a504b4084fe9c4869e4da12ac02e8b44.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "823ab301e228bce311b39d3de4fe0519",
+    "url": "/deboraeric/index.html"
+  },
+  {
+    "revision": "b46395c7e2dee04084e8",
+    "url": "/deboraeric/static/css/main.65c40b73.chunk.css"
+  },
+  {
+    "revision": "7f09c545647c236b13d9",
+    "url": "/deboraeric/static/js/2.b2c740a2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/deboraeric/static/js/2.b2c740a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b46395c7e2dee04084e8",
+    "url": "/deboraeric/static/js/main.55b5f8c2.chunk.js"
+  },
+  {
+    "revision": "e0e992ac17f52cd81e23",
+    "url": "/deboraeric/static/js/runtime-main.6dce3781.js"
+  },
+  {
+    "revision": "46da38df33d636c237fbeed1ef1380f7",
+    "url": "/deboraeric/static/media/us.46da38df.jpg"
+  }
+]);
